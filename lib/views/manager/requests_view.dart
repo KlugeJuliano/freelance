a1e6feadd3d5ff89c100feb08810ace09fc2e5ba
Juliano <kluge233@hotmail.com>
2025-12-28 05:53:10 -0300

    ciração do service de pedidos

--- lib/views/manager/requests_view.dart
+++ lib/views/manager/requests_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:freelance/models/request.dart';
 import 'package:freelance/providers/pedido_provider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
